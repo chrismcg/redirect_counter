61e364e50cd45fbef8ef81e981e6422c05ef5ab0
Chris McGrath <chris@chrismcg.com>
2014-12-05 20:02:06 +0100

    Move twitter code to twitter module and add function to stream tweeks with links

--- lib/redirect_counter.ex
+++ lib/redirect_counter.ex
@@ -1,10 +1,2 @@
 defmodule RedirectCounter do
-  def configure do
-    ExTwitter.configure(
-      consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
-      consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
-      access_token: System.get_env("TWITTER_ACCESS_TOKEN"),
-      access_token_secret: System.get_env("TWITTER_ACCESS_SECRET")
-    )
-  end
 end
---
+++ lib/redirect_counter/twitter.ex
@@ -0,0 +1,17 @@
+defmodule RedirectCounter.Twitter do
+  def configure do
+    ExTwitter.configure(
+      consumer_key: System.get_env("TWITTER_CONSUMER_KEY"),
+      consumer_secret: System.get_env("TWITTER_CONSUMER_SECRET"),
+      access_token: System.get_env("TWITTER_ACCESS_TOKEN"),
+      access_token_secret: System.get_env("TWITTER_ACCESS_SECRET")
+    )
+  end
+
+  def links do
+    configure
+    ExTwitter.stream_filter(track: "link")
+    |> Stream.reject(fn(t) -> t.entities["urls"] == [] end)
+    |> Stream.flat_map(fn(t) -> Enum.map(t.entities["urls"], fn(u) -> u["expanded_url"] end) end)
+  end
+end
