d58c4b2537a60ea71ad701df718339c34768327d
Chris McGrath <chris@chrismcg.com>
2014-12-05 20:18:43 +0100

    Add module to count redirects for url

---
+++ lib/redirect_counter/url.ex
@@ -0,0 +1,15 @@
+defmodule RedirectCounter.URL do
+  def count_redirects(url) do
+    { :ok, response } = HTTPoison.get(url)
+    do_count(response.status_code, response.headers["Location"], 0)
+  end
+
+  defp do_count(status_code, url, redirect_count) when status_code in [301, 302, 307] do
+    { :ok, response } = HTTPoison.get(url)
+    do_count(response.status_code, response.headers["Location"], redirect_count + 1)
+  end
+
+  defp do_count(_status_code, _url, redirect_count) do
+    redirect_count
+  end
+end
